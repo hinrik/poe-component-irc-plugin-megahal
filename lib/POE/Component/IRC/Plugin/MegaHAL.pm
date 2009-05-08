baa01b9feec04f157d9fd635b068e0b8ad90c53e
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2009-05-08 11:56:56 +0000

    Bump version to 0.24

--- lib/POE/Component/IRC/Plugin/MegaHAL.pm
+++ lib/POE/Component/IRC/Plugin/MegaHAL.pm
@@ -8,7 +8,7 @@ use POE::Component::AI::MegaHAL;
 use POE::Component::IRC::Common qw(l_irc matches_mask_array irc_to_utf8 strip_color strip_formatting);
 use POE::Component::IRC::Plugin qw(PCI_EAT_NONE);
 
-our $VERSION = '0.23';
+our $VERSION = '0.24';
 
 sub new {
     my ($package, %args) = @_;
